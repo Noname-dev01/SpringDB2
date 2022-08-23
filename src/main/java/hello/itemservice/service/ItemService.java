72b7f0beab4af632b87b0de1e705fd03aa60e020
noname_dev <noname_dev@naver.com>
2022-08-23 16:49:05 +0900

    Service 인터페이스를 사용하는 이유

--- src/main/java/hello/itemservice/service/ItemService.java
+++ src/main/java/hello/itemservice/service/ItemService.java
@@ -7,6 +7,9 @@ import hello.itemservice.repository.ItemUpdateDto;
 import java.util.List;
 import java.util.Optional;
 
+//Service를 인터페이스를 사용하는 경우는 거의 없고 로직 수정을 하는게 대부분이지만, 이 강의에서는
+//버전을 높여가며 수정이 여러차례 이루어지기 때문에 인터페이스를 도입했다.
+
 public interface ItemService {
 
     Item save(Item item);
