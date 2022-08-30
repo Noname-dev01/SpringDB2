c0ddec7b3c75932b43e8bfdd7b219a543b12ef5c
noname_dev <noname_dev@naver.com>
2022-08-30 17:29:14 +0900

    V1 안 붙힌거 수정

--- src/main/java/hello/itemservice/config/JpaConfig.java
+++ src/main/java/hello/itemservice/config/JpaConfig.java
@@ -1,7 +1,7 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepository;
+import hello.itemservice.repository.jpa.JpaItemRepositoryV1;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import org.springframework.context.annotation.Bean;
@@ -22,5 +22,5 @@ public class JpaConfig {
     public ItemService itemService() { return new ItemServiceV1(itemRepository());}
 
     @Bean
-    public ItemRepository itemRepository() {return new JpaItemRepository(em);}
+    public ItemRepository itemRepository() {return new JpaItemRepositoryV1(em);}
 }
